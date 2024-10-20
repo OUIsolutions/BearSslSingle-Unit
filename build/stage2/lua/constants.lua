cab663a3d952f2a5d79a6d37d03dbf6cb94257cb
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-20 03:15:13 -0300

    at

--- build/stage2/lua/constants.lua
+++ build/stage2/lua/constants.lua
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-DEFAULT_JSON_MODIFIER_MODEL = [[{
-        "replacers":{},
-        "name":"#name#",
-        "private":[]
-    }
-]]
-
-MODIFIER_FOLDER             = "modifier"
-RELEASE_FOLDER              = "release"
-SINGLE_UNIT_FOLDER          = "BearSSLSingleUnit"
-DECLARE_NAME                = "fdeclare"
-DEFINE_NAME                 = "fdefine"
-SILVER_CHAIN_NAME           = "bearssl"
-PROVIDER_GIT                = "https://www.bearssl.org/git/BearSSL"
-=======
 MODIFIER_FOLDER    = "modifier"
 RELEASE_FODER      = "release"
 SINGLE_UNIT_FOLDER = "BearSSLSingleUnit"
@@ -21,4 +5,3 @@ DECLARE_NAME       = "fdeclare"
 DEFINE_NAME        = "fdefine"
 SILVER_CHAIN_NAME  = "bearssl"
 PROVIDER_GIT       = "https://www.bearssl.org/git/BearSSL"
->>>>>>> ctags
--- build/stage2/lua/main.lua
+++ build/stage2/lua/main.lua
@@ -2,31 +2,6 @@ local function main()
     if not dtw.isdir("BearSSL") then
         os.execute("git clone " .. PROVIDER_GIT)
     end
-<<<<<<< HEAD
-
-    local single_unit_point = dtw.concat_path(RELEASE_FOLDER, SINGLE_UNIT_FOLDER)
-    dtw.remove_any(single_unit_point)
-
-    dtw.copy_any_overwriting("BearSSL/inc", single_unit_point .. "/inc")
-
-
-    local src = dtw.newTree_from_hardware("BearSSL/src")
-    main_replace_json = json.load_from_file("main_replace.json")
-
-    src.each(function(current)
-        Aply_file_modification(main_replace_json, current)
-    end)
-    src.commit()
-
-    dtw.copy_any_merging("BearModel", single_unit_point)
-    silver_chain.generate_code(
-        single_unit_point .. "/src",
-        single_unit_point .. "/imports",
-        SILVER_CHAIN_NAME,
-        { "bear", DECLARE_NAME, DEFINE_NAME }
-    )
-    dtw.copy_any_merging("BearModel", single_unit_point)
-=======
     local main_replace = json.load_from_file("main_replace.json")
     local src = dtw.newTree_from_hardware("BearSSL/src")
     dtw.remove_any(RELEASE_FODER)
@@ -46,6 +21,5 @@ local function main()
     )
     dtw.copy_any_merging("BearSSLModel", single_unit_dir)
     Create_summary(modifiers)
->>>>>>> ctags
 end
 main()
