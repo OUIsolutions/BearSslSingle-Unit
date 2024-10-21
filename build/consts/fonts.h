b1d53b5d622c03d8f0200b413a5bb74eebbe8057
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-21 06:21:38 -0300

    att

--- build/consts/fonts.h
+++ build/consts/fonts.h
@@ -2,3 +2,4 @@
 #define CTEXT_ENGINE "https://github.com/OUIsolutions/CTextEngine/releases/download/v2.002/CTextEngine.h"
 #define LUA_CEMBED "https://github.com/OUIsolutions/LuaCEmbed/releases/download/v0.76/LuaCEmbed.h"
 #define UNIVERSAL_GARBAGE "https://github.com/OUIsolutions/Universal-Garbage-Colector/releases/download/v2.003/UniversalGarbage.h"
+#define SILVER_CHAIN "https://github.com/OUIsolutions/SilverChain.git"
--- build/stages/stage1/main.c
+++ build/stages/stage1/main.c
@@ -1,16 +1,25 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "../../consts/all.h"
 #include "../../dependencies/doTheWorld.h"
+#include "../../dependencies/UniversalGarbage.h"
 
 DtwNamespace dtw;
+UniversalGarbage *garbage;
 
 void download_silver_chain(){
     char command[1000]= {0};
-
+    sprintf(command,"%s clone %s",GIT_COMMAND,SILVER_CHAIN);
+    system(command);
+    char *silver_chain_path = dtw.concat_path("dependencies","SilverChain/src");
+    UniversalGarbage_add_simple(garbage,silver_chain_path);
+    dtw.copy_any('')
 }
 
 int main(){
     dtw = newDtwNamespace();
-
+    garbage = newUniversalGarbage();
+    download_silver_chain();
+    UniversalGarbage_free(garbage);
 
 }
