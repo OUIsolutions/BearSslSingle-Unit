4c25687a2b518a4977c5b98947e418140a9bcedb
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-21 07:46:22 -0300

    att

--- build/stage1.c
+++ build/stage1.c
@@ -7,13 +7,10 @@
 DtwNamespace dtw;
 UniversalGarbage *garbage;
 
-void download_git_model(const char *url,const char *name){
-    char curl_command[1000]= {0};
-    sprintf(curl_command,"curl -L %s -o %s.zip",url,name);
-    system(curl_command);
-
-
-
+void download_git_model(const char *url,const char *version,const char *name){
+    char command[1000]= {0};
+    sprintf(command,"%s clone --branch %s %s",GIT_COMMAND,version, url);
+    system(command);
     char *element_src = dtw.concat_path(name,"src");
     UniversalGarbage_add_simple(garbage, element_src);
 
@@ -31,15 +28,13 @@ void download_git_model(const char *url,const char *name){
 int main(){
     dtw = newDtwNamespace();
     garbage = newUniversalGarbage();
-    download_git_model(SILVER_CHAIN, "SilverChain");
-    /*
-    download_git_model(LUA_DO_THE_WORLD, "LuaDoTheWorld");
+    download_git_model(SILVER_CHAIN,SILVER_CHAIN_VERSION, "SilverChain");
+    download_git_model(LUA_DO_THE_WORLD, LUA_DO_THE_WORLD_VERSION,"LuaDoTheWorld");
     char command[1000]= {0};
 
-    sprintf(command,"%s clone %s",GIT_COMMAND,LUA_FLUID_JSON);
+    sprintf(command,"%s clone --branch %s",GIT_COMMAND,LUA_FLUID_JSON_VERSION, LUA_FLUID_JSON);
     dtw.copy_any("LuaFluidJson/src","LuaFluidJson",DTW_NOT_MERGE);
     dtw.remove_any("LuaFluidJson");
-    */
-    UniversalGarbage_free(garbage);
+    UniversalGarbage_free(garbage)
     return 0;
 }
