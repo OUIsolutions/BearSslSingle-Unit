b9de8ea0ef77f56257320e696fb1f70c1123d9a1
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-21 02:31:10 -0300

    att

--- build/stage0/main.c
+++ build/stage0/main.c
@@ -8,8 +8,10 @@ int main(){
     system("rm -rf dependencies");
     system("mkdir dependencies");
     char comand[1000] ={0};
-    sprintf(comand,"curl %s -o dependencies/doTheWorld.h",DO_THE_WORLD);
+    sprintf(comand,"curl -L %s -o dependencies/doTheWorld.h",DO_THE_WORLD);
     system(comand);
+
+
     system("gcc stage1/main.c -o stage.o");
     system("./stage.o");
     return 0;
--- build/stage1/main.c
+++ build/stage1/main.c
@@ -1,6 +1,14 @@
 #include "../fonts.h"
 #include "../dependencies/doTheWorld.h"
 
+DtwNamespace dtw;
 
+void install_CTextEngine(){
+    char commad[1000]={0};
+
+}
 int main(){
+    dtw = newDtwNamespace();
+
+
 }
