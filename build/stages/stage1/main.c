3679626d6f523c2ae412af3f3b21a4e0ac2cacc1
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-21 05:34:30 -0300

    att

--- build/stages/stage1/main.c
+++ build/stages/stage1/main.c
@@ -1,8 +1,13 @@
+#include <stdlib.h>
 #include "../../consts/all.h"
 #include "../../dependencies/doTheWorld.h"
 
 DtwNamespace dtw;
 
+void download_silver_chain(){
+    char command[1000]= {0};
+
+}
 
 int main(){
     dtw = newDtwNamespace();
