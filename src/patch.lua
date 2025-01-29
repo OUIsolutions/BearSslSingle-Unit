11ca90b4b5b1975cae02b54e7c003fd62d6fbb5c
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-28 23:54:46 -0300

    att

--- src/patch.lua
+++ src/patch.lua
@@ -5,8 +5,8 @@ function create_patch()
     for i, item in ipairs(itens) do
         local  old = darwin.dtw.concat_path("origin",item)
         local  new = darwin.dtw.concat_path("transformed",item)
-        local  patch = darwin.dtw.concat_path("patch",item)
-        dtw.write_file(patch,"") -- just to ensure the file exists        
+        local  patch = darwin.dtw.concat_path("patch",item..".patch")
+        darwin.dtw.write_file(patch,"") -- just to ensure the file exists        
         os.execute("diff  "..old.." "..new.." > "..patch)
     end
 end 
