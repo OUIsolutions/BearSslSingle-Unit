a88bf6f90a5cc606130d5c99bcb166f9e5f77796
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-29 03:15:37 -0300

    att

--- src/main.lua
+++ src/main.lua
@@ -3,15 +3,7 @@
 function main()
     
 
-   --iterate over transformmed 
-   local transformed = darwin.dtw.list_files_recursively("transformed",true)
-   for i, file in ipairs(transformed) do
-      print(file)
-       local content = darwin.dtw.load_file(file)
-       local new_content =replace_string(content, '#include "inner.h"', "")
-       darwin.dtw.write_file(file, new_content)
-   end
-
+  
 
    if darwin.argv.one_of_args_exist("update_bear")  then
         update_bear()
