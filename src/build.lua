921a62f5d025aa007b66adf64883169593e18e20
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-29 00:25:07 -0300

    att

--- src/build.lua
+++ src/build.lua
@@ -2,5 +2,16 @@
 
 function build()
     aply_patch()
-    
+    local itens = darwin.dtw.list_files_recursively(OUTPUT_DIR,true)
+    for i, item in ipairs(itens) do
+        local path = darwin.dtw.newPath(item)
+        local extension = path.get_extension()
+        if extension == "c" or extension == "cpp" then
+            local name = path.get_name()
+            local new_name = "fdefine"..name
+            path.set_name(new_name)
+            darwin.dtw.move_any_overwriting(item,path)
+            
+        end
+    end
 end 
