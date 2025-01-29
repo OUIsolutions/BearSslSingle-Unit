039c3f99d6d1fa383ca8559f464d0c70236314b8
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-29 02:21:36 -0300

    att

--- transformed/src/symcipher/poly1305_i15.c
+++ transformed/src/symcipher/poly1305_i15.c
@@ -42,7 +42,7 @@ static const uint16_t P1305[] = {
 /*
  * -p mod 2^15.
  */
-#define P0I   0x4CCD
+#define poly_1305_i15_P0I   0x4CCD
 
 /*
  * R^2 mod p, for conversion to Montgomery representation (R = 2^135,
@@ -100,7 +100,7 @@ i15_c_poly1305_inner(uint16_t *a, const uint16_t *r, const void *data, size_t le
 		/*
 		 * Multiply by r, result is the new accumulator value.
 		 */
-		br_i15_montymul(a, b, r, P1305, P0I);
+		br_i15_montymul(a, b, r, P1305, poly_1305_i15_P0I);
 
 		buf += 16;
 		len -= 16;
@@ -176,7 +176,7 @@ br_poly1305_i15_run(const void *key, const void *iv,
 	/*
 	 * Convert 'r' to Montgomery representation.
 	 */
-	br_i15_montymul(r, t, R2, P1305, P0I);
+	br_i15_montymul(r, t, R2, P1305, poly_1305_i15_P0I);
 
 	/*
 	 * Accumulator is 0.
