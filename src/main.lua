66d69a85304f47792a5fd2b4cfd6855b53fde357
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-28 23:45:42 -0300

    att

--- src/main.lua
+++ src/main.lua
@@ -2,8 +2,17 @@
 
 function main()
     
-    if darwin.argv.one_of_args_exist("update_bear")  then
-        update_bear()
+    local itens = darwin.dtw.list_files_recursively("transformed/",true)
+    for i, item in ipairs(itens) do
+        local content = darwin.dtw.read_file(item)
+        local formmated = string.gsub(content, '#include "inner.h"', "")
+        darwin.dtw.write_file(item, formmated)
     end
+
+
+
+   -- if darwin.argv.one_of_args_exist("update_bear")  then
+   --     update_bear()
+   -- end
 
 end 
