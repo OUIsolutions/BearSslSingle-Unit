536d56173892d7f9310065633c911a8085e21ea8
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-29 07:23:42 -0300

    att

--- src/main.lua
+++ src/main.lua
@@ -3,14 +3,14 @@
 function main()
     
 
-   local transformmed = darwin.dtw.list_files_recursively("transformed",true)
-   for i, v in ipairs(transformmed) do
-       local file = transformmed[i]
-       local content= darwin.dtw.load_file(file)
-      var = 'C255_R2'
-      local replaced = string.gsub(content, var, "BEAR_SINGLE_UNITY_FILE"..var)
-      darwin.dtw.write_file(file, replaced)
-   end
+   --local transformmed = darwin.dtw.list_files_recursively("transformed",true)
+   --for i, v in ipairs(transformmed) do
+   --    local file = transformmed[i]
+   --    local content= darwin.dtw.load_file(file)
+   --   var = 'C255_R2'
+   --   local replaced = string.gsub(content, var, "BEAR_SINGLE_UNITY_FILE"..var)
+   --   darwin.dtw.write_file(file, replaced)
+   --end
 
    if darwin.argv.one_of_args_exist("update_bear")  then
         update_bear()
