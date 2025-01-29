6b95beeef1d906980fc740bf1a2b1e8451113ea9
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-29 03:12:59 -0300

    at

--- src/algo.lua
+++ src/algo.lua
@@ -2,15 +2,16 @@ function starts_with(str, prefix)
     return str:sub(1, #prefix) == prefix
 end
 function replace_string(str, old, new)
-    local new_str = ""
+    local new_str = {}
     local i = 1
     while i <= #str do
         if starts_with(str:sub(i), old) then
-            new_str = new_str .. new
+            new_str[#new_str + 1] = new
             i = i + #old
         else
-            new_str = new_str .. str:sub(i, i)
+            new_str[#new_str + 1] = str:sub(i, i)
             i = i + 1
         end
     end
+    return table.concat(new_str)
 end
--- src/main.lua
+++ src/main.lua
@@ -6,8 +6,10 @@ function main()
    --iterate over transformmed 
    local transformed = darwin.dtw.list_files_recursively("transformed",true)
    for i, file in ipairs(transformed) do
+      print(file)
        local content = darwin.dtw.load_file(file)
        local new_content =replace_string(content, '#include "inner.h"', "")
+       darwin.dtw.write_file(file, new_content)
    end
 
 
