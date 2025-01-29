0b4a8bef6eb507e40caf7c79664ad3e38a31a6db
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-28 22:56:47 -0300

    at

--- src/update_bear.lua
+++ src/update_bear.lua
@@ -2,6 +2,9 @@
 
 function update_bear()
     print("updatting bear")
-    darwin.dtw.remove_any("bear")
+    darwin.dtw.remove_any("BearSSL")
     os.execute("git clone "..BEARSS_URL)
+    darwin.dtw.move_any_overwriting("BearSSL/inc","origin/inc")
+    darwin.dtw.move_any_overwriting("BearSSL/src","origin/src")
+    darwin.dtw.remove_any("BearSSL")
 end
