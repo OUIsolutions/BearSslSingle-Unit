2b3d287c8969d7dd2498290d160c7d224ac4e5f3
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-29 05:42:27 -0300

    at

--- transformed/src/hash/sha1.c
+++ transformed/src/hash/sha1.c
@@ -24,12 +24,12 @@
 
 
 
-#define F(B, C, D)     ((((C) ^ (D)) & (B)) ^ (D))
-#define G(B, C, D)     ((B) ^ (C) ^ (D))
-#define H(B, C, D)     (((D) & (C)) | (((D) | (C)) & (B)))
-#define I(B, C, D)     G(B, C, D)
+#define (BEAR_SINGLE_UNITY_FILE)F(B, C, D)     ((((C) ^ (D)) & (B)) ^ (D))
+#define (BEAR_SINGLE_UNITY_FILE)G(B, C, D)     ((B) ^ (C) ^ (D))
+#define (BEAR_SINGLE_UNITY_FILE)H(B, C, D)     (((D) & (C)) | (((D) | (C)) & (B)))
+#define (BEAR_SINGLE_UNITY_FILE)I(B, C, D)     (BEAR_SINGLE_UNITY_FILE)G(B, C, D)
 
-#define ROTL(x, n)    (((x) << (n)) | ((x) >> (32 - (n))))
+#define (BEAR_SINGLE_UNITY_FILE)ROTL(x, n)    (((x) << (n)) | ((x) >> (32 - (n))))
 
 #define K1     ((uint32_t)0x5A827999)
 #define K2     ((uint32_t)0x6ED9EBA1)
@@ -57,36 +57,36 @@ br_sha1_round(const unsigned char *buf, uint32_t *val)
 	br_range_dec32be(m, 16, buf);
 	for (i = 16; i < 80; i ++) {
 		uint32_t x = m[i - 3] ^ m[i - 8] ^ m[i - 14] ^ m[i - 16];
-		m[i] = ROTL(x, 1);
+		m[i] = (BEAR_SINGLE_UNITY_FILE)ROTL(x, 1);
 	}
 
 	for (i = 0; i < 20; i += 5) {
-		e += ROTL(a, 5) + F(b, c, d) + K1 + m[i + 0]; b = ROTL(b, 30);
-		d += ROTL(e, 5) + F(a, b, c) + K1 + m[i + 1]; a = ROTL(a, 30);
-		c += ROTL(d, 5) + F(e, a, b) + K1 + m[i + 2]; e = ROTL(e, 30);
-		b += ROTL(c, 5) + F(d, e, a) + K1 + m[i + 3]; d = ROTL(d, 30);
-		a += ROTL(b, 5) + F(c, d, e) + K1 + m[i + 4]; c = ROTL(c, 30);
+		e += (BEAR_SINGLE_UNITY_FILE)ROTL(a, 5) + (BEAR_SINGLE_UNITY_FILE)F(b, c, d) + K1 + m[i + 0]; b = (BEAR_SINGLE_UNITY_FILE)ROTL(b, 30);
+		d += (BEAR_SINGLE_UNITY_FILE)ROTL(e, 5) + (BEAR_SINGLE_UNITY_FILE)F(a, b, c) + K1 + m[i + 1]; a = (BEAR_SINGLE_UNITY_FILE)ROTL(a, 30);
+		c += (BEAR_SINGLE_UNITY_FILE)ROTL(d, 5) + (BEAR_SINGLE_UNITY_FILE)F(e, a, b) + K1 + m[i + 2]; e = (BEAR_SINGLE_UNITY_FILE)ROTL(e, 30);
+		b += (BEAR_SINGLE_UNITY_FILE)ROTL(c, 5) + (BEAR_SINGLE_UNITY_FILE)F(d, e, a) + K1 + m[i + 3]; d = (BEAR_SINGLE_UNITY_FILE)ROTL(d, 30);
+		a += (BEAR_SINGLE_UNITY_FILE)ROTL(b, 5) + (BEAR_SINGLE_UNITY_FILE)F(c, d, e) + K1 + m[i + 4]; c = (BEAR_SINGLE_UNITY_FILE)ROTL(c, 30);
 	}
 	for (i = 20; i < 40; i += 5) {
-		e += ROTL(a, 5) + G(b, c, d) + K2 + m[i + 0]; b = ROTL(b, 30);
-		d += ROTL(e, 5) + G(a, b, c) + K2 + m[i + 1]; a = ROTL(a, 30);
-		c += ROTL(d, 5) + G(e, a, b) + K2 + m[i + 2]; e = ROTL(e, 30);
-		b += ROTL(c, 5) + G(d, e, a) + K2 + m[i + 3]; d = ROTL(d, 30);
-		a += ROTL(b, 5) + G(c, d, e) + K2 + m[i + 4]; c = ROTL(c, 30);
+		e += (BEAR_SINGLE_UNITY_FILE)ROTL(a, 5) + (BEAR_SINGLE_UNITY_FILE)G(b, c, d) + K2 + m[i + 0]; b = (BEAR_SINGLE_UNITY_FILE)ROTL(b, 30);
+		d += (BEAR_SINGLE_UNITY_FILE)ROTL(e, 5) + (BEAR_SINGLE_UNITY_FILE)G(a, b, c) + K2 + m[i + 1]; a = (BEAR_SINGLE_UNITY_FILE)ROTL(a, 30);
+		c += (BEAR_SINGLE_UNITY_FILE)ROTL(d, 5) + (BEAR_SINGLE_UNITY_FILE)G(e, a, b) + K2 + m[i + 2]; e = (BEAR_SINGLE_UNITY_FILE)ROTL(e, 30);
+		b += (BEAR_SINGLE_UNITY_FILE)ROTL(c, 5) + (BEAR_SINGLE_UNITY_FILE)G(d, e, a) + K2 + m[i + 3]; d = (BEAR_SINGLE_UNITY_FILE)ROTL(d, 30);
+		a += (BEAR_SINGLE_UNITY_FILE)ROTL(b, 5) + (BEAR_SINGLE_UNITY_FILE)G(c, d, e) + K2 + m[i + 4]; c = (BEAR_SINGLE_UNITY_FILE)ROTL(c, 30);
 	}
 	for (i = 40; i < 60; i += 5) {
-		e += ROTL(a, 5) + H(b, c, d) + K3 + m[i + 0]; b = ROTL(b, 30);
-		d += ROTL(e, 5) + H(a, b, c) + K3 + m[i + 1]; a = ROTL(a, 30);
-		c += ROTL(d, 5) + H(e, a, b) + K3 + m[i + 2]; e = ROTL(e, 30);
-		b += ROTL(c, 5) + H(d, e, a) + K3 + m[i + 3]; d = ROTL(d, 30);
-		a += ROTL(b, 5) + H(c, d, e) + K3 + m[i + 4]; c = ROTL(c, 30);
+		e += (BEAR_SINGLE_UNITY_FILE)ROTL(a, 5) + (BEAR_SINGLE_UNITY_FILE)H(b, c, d) + K3 + m[i + 0]; b = (BEAR_SINGLE_UNITY_FILE)ROTL(b, 30);
+		d += (BEAR_SINGLE_UNITY_FILE)ROTL(e, 5) + (BEAR_SINGLE_UNITY_FILE)H(a, b, c) + K3 + m[i + 1]; a = (BEAR_SINGLE_UNITY_FILE)ROTL(a, 30);
+		c += (BEAR_SINGLE_UNITY_FILE)ROTL(d, 5) + (BEAR_SINGLE_UNITY_FILE)H(e, a, b) + K3 + m[i + 2]; e = (BEAR_SINGLE_UNITY_FILE)ROTL(e, 30);
+		b += (BEAR_SINGLE_UNITY_FILE)ROTL(c, 5) + (BEAR_SINGLE_UNITY_FILE)H(d, e, a) + K3 + m[i + 3]; d = (BEAR_SINGLE_UNITY_FILE)ROTL(d, 30);
+		a += (BEAR_SINGLE_UNITY_FILE)ROTL(b, 5) + (BEAR_SINGLE_UNITY_FILE)H(c, d, e) + K3 + m[i + 4]; c = (BEAR_SINGLE_UNITY_FILE)ROTL(c, 30);
 	}
 	for (i = 60; i < 80; i += 5) {
-		e += ROTL(a, 5) + I(b, c, d) + K4 + m[i + 0]; b = ROTL(b, 30);
-		d += ROTL(e, 5) + I(a, b, c) + K4 + m[i + 1]; a = ROTL(a, 30);
-		c += ROTL(d, 5) + I(e, a, b) + K4 + m[i + 2]; e = ROTL(e, 30);
-		b += ROTL(c, 5) + I(d, e, a) + K4 + m[i + 3]; d = ROTL(d, 30);
-		a += ROTL(b, 5) + I(c, d, e) + K4 + m[i + 4]; c = ROTL(c, 30);
+		e += (BEAR_SINGLE_UNITY_FILE)ROTL(a, 5) + (BEAR_SINGLE_UNITY_FILE)I(b, c, d) + K4 + m[i + 0]; b = (BEAR_SINGLE_UNITY_FILE)ROTL(b, 30);
+		d += (BEAR_SINGLE_UNITY_FILE)ROTL(e, 5) + (BEAR_SINGLE_UNITY_FILE)I(a, b, c) + K4 + m[i + 1]; a = (BEAR_SINGLE_UNITY_FILE)ROTL(a, 30);
+		c += (BEAR_SINGLE_UNITY_FILE)ROTL(d, 5) + (BEAR_SINGLE_UNITY_FILE)I(e, a, b) + K4 + m[i + 2]; e = (BEAR_SINGLE_UNITY_FILE)ROTL(e, 30);
+		b += (BEAR_SINGLE_UNITY_FILE)ROTL(c, 5) + (BEAR_SINGLE_UNITY_FILE)I(d, e, a) + K4 + m[i + 3]; d = (BEAR_SINGLE_UNITY_FILE)ROTL(d, 30);
+		a += (BEAR_SINGLE_UNITY_FILE)ROTL(b, 5) + (BEAR_SINGLE_UNITY_FILE)I(c, d, e) + K4 + m[i + 4]; c = (BEAR_SINGLE_UNITY_FILE)ROTL(c, 30);
 	}
 
 	val[0] += a;
