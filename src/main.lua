e1f93b8ba15462434f15e6f3cee2f3791490fb42
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-29 05:29:14 -0300

    att

--- src/main.lua
+++ src/main.lua
@@ -7,9 +7,9 @@ function main()
    for i, v in ipairs(transformmed) do
        local file = transformmed[i]
        local content= darwin.dtw.load_file(file)
-      var = 'cond_negate'
-      local replaced = string.gsub(content, var, "(BEAR_SINGLE_UNITY_FILE)"..var)
-       darwin.dtw.write_file(file, replaced)
+     -- var = 'cond_negate'
+      --local replaced = string.gsub(content, var, "(BEAR_SINGLE_UNITY_FILE)"..var)
+      -- darwin.dtw.write_file(file, replaced)
    end
 
    if darwin.argv.one_of_args_exist("update_bear")  then
--- transformed/src/hash/sha2small.c
+++ transformed/src/hash/sha2small.c
@@ -46,7 +46,7 @@ const uint32_t br_sha256_IV[8] = {
 	0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
 };
 
-static const uint32_t K[64] = {
+static const uint32_t (BEAR_SINGLE_UNITY_FILE)K[64] = {
 	0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
 	0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
 	0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
@@ -72,7 +72,7 @@ br_sha2small_round(const unsigned char *buf, uint32_t *val)
 
 #define SHA2_STEP(A, B, C, D, E, F, G, H, j)   do { \
 		uint32_t T1, T2; \
-		T1 = H + BSG2_1(E) + CH(E, F, G) + K[j] + w[j]; \
+		T1 = H + BSG2_1(E) + CH(E, F, G) + (BEAR_SINGLE_UNITY_FILE)K[j] + w[j]; \
 		T2 = BSG2_0(A) + MAJ(A, B, C); \
 		D += T1; \
 		H = T1 + T2; \
