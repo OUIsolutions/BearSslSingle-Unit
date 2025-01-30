bdcdd7db816647e1461d470d9f6c42bf0fc11f2b
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-30 03:01:33 -0300

    att

--- src/release.lua
+++ src/release.lua
@@ -5,4 +5,11 @@ function generate_release()
         darwin.dtw.concat_path(OUTPUT_DIR,"one.c")
    )
    darwin.dtw.write_file(darwin.dtw.concat_path(RELEASE_FOLDER,ONE_NAME),one_result)
+
+
+   local only_header_path = darwin.dtw.concat_path(OUTPUT_DIR,"src/fdeclare.inner.h")
+   local only_headder = darwin.camalgamator.generate_amalgamation(only_header_path)
+   darwin.dtw.write_file(darwin.dtw.concat_path(RELEASE_FOLDER,ONLY_HEADER_NAME),only_headder)
+
+
 end 
