df7ceac137a63601a2767670ee39eef82e6acf58
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-30 05:12:00 -0300

    att

--- src/conf.lua
+++ src/conf.lua
@@ -1,7 +1,7 @@
 
 
 BEARSS_URL = "https://www.bearssl.org/git/BearSSL"
-PATCH_DIR = "debug_patches"
+PATCH_DIR = "patches"
 OUTPUT_DIR = "BearSSlOutput"
 PROJECT_NAME = "BearSSLSingleUnit"
 ONE_NAME ="BearSSLSingleUnitOne.c"
