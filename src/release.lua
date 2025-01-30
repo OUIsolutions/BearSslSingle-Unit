e0510c2ac7a092aff16bb1aa2d45034e8f58e58f
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-30 04:07:07 -0300

    att

--- src/release.lua
+++ src/release.lua
@@ -21,8 +21,11 @@ function generate_release()
             return "include-once" 
         end
    )    
-   only_define_content = "#define BR_ENABLE_INTRINSICS   1\n"..only_define_content
-   only_define_content = '#include "BearSSLSingleUnit.h"\n'..only_define_content
+   local pre_include = "#define BR_ENABLE_INTRINSICS   1\n"
+   pre_include = pre_include.."#include \"BearSSLSingleUnit.h\"\n"
+   only_define_content = pre_include..only_define_content
+
+  
    darwin.dtw.write_file(darwin.dtw.concat_path(RELEASE_FOLDER,ONLY_DEFINE_NAME),only_define_content)
 
    local zip_path = darwin.dtw.concat_path(RELEASE_FOLDER,ZIP_NAME)
