248b3223dc0e532fc6f90944dadbdb9beb8e6b7a
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-30 05:17:32 -0300

    att

--- one.c
+++
@@ -1,6 +0,0 @@
-
-#ifndef BearSingleUnit
-#define BearSingleUnit
-#define BR_ENABLE_INTRINSICS   1
-#include "imports/imports.fdefine.h"
-#endif
--- teste.lua
+++
@@ -1,5 +0,0 @@
-local name = "/aaa/bbb/ccc/ddd/eee/fff/ggg//hhh/iii/jjj.tx"
-local sanitized = string.gsub(name, "/","_")
-sanitized = string.gsub(sanitized,"__","_")
-sanitized = string.gsub(sanitized,"%.","_")
-print(sanitized)
