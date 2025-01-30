277532cd73e6b3ba0086f848a097364bc3facf8f
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-30 03:09:38 -0300

    att

--- src/release.lua
+++ src/release.lua
@@ -11,5 +11,14 @@ function generate_release()
    local only_headder = darwin.camalgamator.generate_amalgamation(only_header_path)
    darwin.dtw.write_file(darwin.dtw.concat_path(RELEASE_FOLDER,ONLY_HEADER_NAME),only_headder)
 
+   local only_define_path = darwin.dtw.concat_path(OUTPUT_DIR,"imports/imports.fdefine.h")
+   local only_define_content = darwin.camalgamator.generate_amalgamation_with_callback(
+        only_define_path,
+        function (import,path)
+            print(import)
+            return true 
+        end
+   )    
+    darwin.dtw.write_file(darwin.dtw.concat_path(RELEASE_FOLDER,ONLY_DEFINE_NAME),only_define_content)
 
 end 
